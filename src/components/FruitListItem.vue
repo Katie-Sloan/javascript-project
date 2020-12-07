6ad5b3041661280e3a5ee8ed3fb5c526359b028c
Katie Sloan <katie.a.sloan@gmail.com>
2020-12-07 11:22:36 +0000

    Populated FruitListItem.vue

---
+++ src/components/FruitListItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <li v-on:click="handleClick">{{fruit.name}}</li>
+</template>
+
+<script>
+import { eventBus } from '..main.js'
+export default {
+    name: 'fruit-list-item',
+    props: ['fruit'],
+    methods: {
+        handleClick(){
+            eventBus.$emit('fruit-selected', this.fruit)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
