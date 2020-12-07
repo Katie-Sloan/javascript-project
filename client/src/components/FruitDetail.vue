03f43dd5b10f74f9d8e00aa9482aeb24b0ce9711
Katie Sloan <katie.a.sloan@gmail.com>
2020-12-07 15:28:17 +0000

    Added select fruit functionality

--- client/src/components/FruitDetail.vue
+++ client/src/components/FruitDetail.vue
@@ -2,32 +2,27 @@
   <div v-if="fruit">
       <h2>{{fruit.name}}</h2>
       <br>
-      <p>Orders contain different families. Each order is divided into families. 
-          <span>Order</span>: {{fruit.order}}
-      </p>
-      <p>Families are plants with many botanical features in common. There are roughly 150-500 different families of plants (the botanists can't agree on how many)!
-          <span>Family</span>: {{fruit.family}}
-      </p>
-      <p>Just think of plant names like your last name followed by your first name: Harper John, except that plants are called by their Genus and species: Rosa rugosa.
-          <span>Genus</span>: {{fruit.genus}}
-      </p>
+      <h4><b>Order:</b> {{fruit.order}} </h4> 
+      <p>Orders contain different families. Each order is divided into families.</p>
+      <h4><b>Family:</b> {{fruit.family}} </h4> 
+      <p>Families are plants with many botanical features in common. There are roughly 150-500 different families of plants (the botanists can't agree on how many)!</p>
+      <h4><b>Genus:</b> {{fruit.genus}} </h4>
+      <p>Just think of plant names like your last name followed by your first name: Harper John, except that plants are called by their Genus and species: Rosa rugosa.</p>
       <p>
-          <span>Nutrition (per 100g)</span>: 
-          Carbohydrates: {{fruit.nutrition.carbohydrates}}g
-          Protein: {{fruit.nutrition.protein}}g
-          Fat: {{fruit.nutrition.fat}}g
-          Calories: {{fruit.nutrition.calories}}kcal
-          Sugar: {{fruit.nutrition.sugar}}g  
+          <span><b>Nutrition (per 100g)</b></span>: 
+          Carbohydrates: {{fruit.nutritions.carbohydrates}}g
+          Protein: {{fruit.nutritions.protein}}g
+          Fat: {{fruit.nutritions.fat}}g
+          Calories: {{fruit.nutritions.calories}}kcal
+          Sugar: {{fruit.nutritions.sugar}}g  
       </p>
-
-
-
   </div>
 </template>
 
 <script>
 export default {
-
+    name: 'fruit-detail',
+    props: ['fruit']
 }
 </script>
 
