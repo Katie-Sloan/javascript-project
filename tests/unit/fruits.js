6d0eab2455153caa22e6e2a31988386ccc61156f
Katie Sloan <katie.a.sloan@gmail.com>
2020-12-07 10:46:46 +0000

    Populated fruits.js

---
+++ tests/unit/fruits.js
@@ -0,0 +1,27 @@
+export const fruitsData = [{
+    "genus": "Malus", 
+    "name":"Apple", 
+    "family":"Rosaceae",
+    "order": "Rosales",
+    "nutritions":{ 
+        "carbohydrates": 11.4,
+        "protein": 0.3, 
+        "fat": 0.4, 
+        "calories": 52, 
+        "sugar": 10.3 
+    }
+},
+{
+    "genus": "Prunus",
+    "name": "Apricot",
+    "family": "Rosaceae",
+    "order": "Rosales",
+    "nutritions": {
+        "carbohydrates": 3.9,
+        "protein": 0.5,
+        "fat": 0.1,
+        "calories": 15,
+        "sugar": 3.2
+    }
+}
+];
