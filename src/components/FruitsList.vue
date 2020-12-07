6f1155459e70d87825e390c62bff38013988d064
Katie Sloan <katie.a.sloan@gmail.com>
2020-12-07 11:19:23 +0000

    Populated FruitsList.vue

---
+++ src/components/FruitsList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <ul>
+          <fruit-list-item v-for="(fruit, index) in fruits" :fruit="fruit" :key="index"></fruit-list-item>
+      </ul>
+  </div>
+</template>
+
+<script>
+import FruitListItem from './FruitListItem.vue'
+
+export default {
+    name: 'fruits-list',
+    props: ['fruits'],
+    components: {
+        "fruit-list-item": FruitListItem
+    }
+}
+</script>
+
+<style>
+
+</style>
