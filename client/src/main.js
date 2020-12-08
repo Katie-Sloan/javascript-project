637e046d9d7f13a932ceadbe5c8da2cbb97c7324
StuuGraham <stuartgraham7@hotmail.co.uk>
2020-12-08 10:00:59 +0000

    added leaflet css import and Vue.components created for LMap, LTileLayer & LMarker

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 Vue.config.productionTip = false
 
 export const eventBus = new Vue();
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
 
 new Vue({
   render: h => h(App),
